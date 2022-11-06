2ebcb5f04211ed2ca81e22d02aa35b5b685a4636
Kasun Kodagoda <kvkrusl@gmail.com>
2022-11-07 01:39:42 +0530

    Added the initial project structure.

---
+++ src/FriendOrganizer/FriendOrganizer.Model/Friend.cs
@@ -0,0 +1,10 @@
+﻿namespace FriendOrganizer.Model;
+
+public class Friend
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+
+}
