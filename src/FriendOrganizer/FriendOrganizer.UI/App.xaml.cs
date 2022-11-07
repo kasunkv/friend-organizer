cd6068a35d26acebb891ca5fd2a8e73fa4aa5ca6
Kasun Kodagoda <kvkrusl@gmail.com>
2022-11-07 13:49:18 +0530

    Switched to using .Net core dependency injection instead of Autofac

--- src/FriendOrganizer/FriendOrganizer.UI/App.xaml.cs
+++ src/FriendOrganizer/FriendOrganizer.UI/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Autofac;
-using FriendOrganizer.UI.Startup;
+﻿using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 using System.Windows;
 
 namespace FriendOrganizer.UI
@@ -9,13 +13,33 @@ namespace FriendOrganizer.UI
     /// </summary>
     public partial class App : Application
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            var bootstrapper = new Bootstrapper();
-            var container = bootstrapper.Bootstrap();
 
-            var mainWindow = container.Resolve<MainWindow>();
+        public IServiceProvider ServiceProvider { get; set; }
+        public IConfiguration Configuration { get; set; }
+
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
             mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<MainWindow>();
+            services.AddTransient<IFriendDataService, FriendDataService>();
         }
     }
 }
