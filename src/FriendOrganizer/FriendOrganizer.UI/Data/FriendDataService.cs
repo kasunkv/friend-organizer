9ce7c99cd45f70b98539701fb26c52c1da33395d
Kasun Kodagoda <kvkrusl@gmail.com>
2022-11-07 10:59:48 +0530

    Added the sample data service implementation

---
+++ src/FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,15 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data;
+
+public class FriendDataService : IFriendDataService
+{
+    public IEnumerable<Friend> GetAll()
+    {
+        yield return new Friend { FirstName = "Kasun", LastName = "Kodagoda" };
+        yield return new Friend { FirstName = "Oreliya", LastName = "Fernando" };
+        yield return new Friend { FirstName = "John", LastName = "Doe" };
+        yield return new Friend { FirstName = "Jane", LastName = "Smith" };
+    }
+}
